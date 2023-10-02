fd38a0bccb1487d9d11a34a35246e32452817757
MarcusTheCode <marcusbyllemos@gmail.com>
2023-10-02 10:08:27 +0200

    Added functions to FrontendServiceInterface and FrontendService

--- src/main/java/dk/bm/fido/auth/services/FrontendServiceInterface.java
+++ src/main/java/dk/bm/fido/auth/services/FrontendServiceInterface.java
@@ -1,7 +1,13 @@
 package dk.bm.fido.auth.services;
 
+import dk.bm.fido.auth.dtos.UserAccountDto;
+import dk.bm.fido.auth.dtos.WSO2UserAccountDto;
+import dk.bm.fido.auth.models.UserAccount;
 import org.springframework.stereotype.Component;
 
 @Component
 public interface FrontendServiceInterface {
+    UserAccountDto authenticateUser(UserAccountDto uSerAccountDto);
+    WSO2UserAccountDto checkUserAuthentication(WSO2UserAccountDto wso2UserAccountDto);
+    UserAccountDto checkUserAuthentication(UserAccountDto userAccountDto);
 }
