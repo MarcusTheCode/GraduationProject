354e6b6ae2a5de7d683eb437ed3c3511885c2135
MarcusTheCode <marcusbyllemos@gmail.com>
2023-10-24 13:38:48 +0200

    Added comments to all controllers and the last comment for WSO2Service

--- src/main/java/dk/bm/fido/auth/controllers/FidoController.java
+++ src/main/java/dk/bm/fido/auth/controllers/FidoController.java
@@ -1,8 +1,7 @@
 package dk.bm.fido.auth.controllers;
 
-import dk.bm.fido.auth.external.dtos.DeviceDto;
 import dk.bm.fido.auth.external.services.WSO2Service;
-import dk.bm.fido.auth.services.FrontEndServiceSupreme;
+import dk.bm.fido.auth.services.FrontEndService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
@@ -19,35 +18,56 @@ public class FidoController {
         this.wso2Service = wso2Service;
     }
 
+    /**
+     * Function for retrieving the users FIDO2 devices and returning them with the html page
+     * @param model container for attributes between backend and frontend
+     * @param authentication identification of the current user
+     * @param authorizedClient the authorized client pulled from the authorization header; it is pulled out separately for simplicity
+     * @return the FIDO2 devices list html, with the FIDO2 devices added to the model
+     */
     @GetMapping("fidoDevices")
     public String fidoDevices(
             Model model,
             Authentication authentication,
             @RegisteredOAuth2AuthorizedClient("wso2") OAuth2AuthorizedClient authorizedClient
     ) {
-        FrontEndServiceSupreme.setAuthenticated(authentication, model);
+        FrontEndService.setAuthenticated(authentication, model);
         model.addAttribute(
                 "devices" ,
-                wso2Service.getUserDevices(FrontEndServiceSupreme.getBearerToken(authorizedClient))
+                wso2Service.getUserDevices(FrontEndService.getBearerToken(authorizedClient))
         );
         return "fidoDevices";
     }
 
+    /**
+     * Function used to delete a FIDO2 device/login from the users account
+     * @param credential the id of the FIDO2 credential to be deleted
+     * @param authorizedClient the authorized client pulled from the authorization header
+     * @return redirect to the fidoDevices html page
+     */
     @PostMapping(value="fidoDevices/deleteDevice/{credential}") //Delete not supported by forms
     public String deleteFidoDevice(
             @PathVariable String credential,
             @RegisteredOAuth2AuthorizedClient("wso2") OAuth2AuthorizedClient authorizedClient
     ) {
-        wso2Service.deleteDeviceCredential(FrontEndServiceSupreme.getBearerToken(authorizedClient), credential);
+        wso2Service.deleteDeviceCredential(FrontEndService.getBearerToken(authorizedClient), credential);
         return "redirect:/fidoDevices";
     }
+
+    /**
+     * Mapping for updating the name of the FIDO2 device
+     * @param credential the id of the FIDO2 credential to be updated
+     * @param newName the new name of the device
+     * @param authorizedClient the authorized client pulled from the authorization header
+     * @return redirect to the fidoDevices html page
+     */
     @PostMapping("fidoDevices/editDevice/{credential}/{newName}")
     public String editFidoDevice(
             @PathVariable String credential,
             @PathVariable String newName,
             @RegisteredOAuth2AuthorizedClient("wso2") OAuth2AuthorizedClient authorizedClient
     ) {
-        wso2Service.editDeviceName(FrontEndServiceSupreme.getBearerToken(authorizedClient), credential, newName);
+        wso2Service.editDeviceName(FrontEndService.getBearerToken(authorizedClient), credential, newName);
         return "redirect:/fidoDevices";
     }
 }
--- src/main/java/dk/bm/fido/auth/controllers/LoginController.java
+++ src/main/java/dk/bm/fido/auth/controllers/LoginController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
+    /**
+     * Function for redirecting to wso2 login page
+     * @return redirect to login page
+     */
     @GetMapping("/login")
     public String login () {
         return "redirect:/oauth2/authorization/wso2";
